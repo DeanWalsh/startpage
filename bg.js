97cfa35ed89827fc222ae0d67828a9b46f42fe41
DeanWalsh <deanochambers@gmail.com>
2020-06-14 13:43:51 +0100

    script preload link added

---
+++ bg.js
@@ -0,0 +1,31 @@
+//  var currentTime = new Date().getHours();
+//  if ( currentTime> 6 | currentTime<9) {
+//      if(document.body) {
+//          document.getElementById('Body').className="bg1";}
+//  }
+//  else if (currentTime>=9 &&currentTime < 20) {
+//      document.body.style.backgroundImage = url(/img/05-Mid-Afternoon.png) 
+//  }
+//  else if (currentTime>20 &&currentTime < 24) {
+//      if(document.body) {
+//          document.getElementById('Body').className="bg3";}
+//  }
+//  else {
+//      if(document.body) {
+//          document.getElementById('Body').className="bg4";}
+//  }
+
+//  $(document).ready(function(){
+//  	var d = new Date();
+//  	var n = d.getHours();
+//  	if (n > 19 || n < 6)
+//  	//    If time is after 7PM or before 6AM, apply night theme to ‘body’
+//  	  document.body.className = "night";
+//  	else if (n > 16 && n < 19)
+//  	//    If time is between 4PM – 7PM sunset theme to ‘body’
+//  	  document.body.className = "evening";
+//  	else
+//  	//    Else use ‘day’ theme
+//  	  document.body.className = "morning";
+//  });
+
